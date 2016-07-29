61ab8fb95997df55a72a149c45651f8a4eb73c7f
Kyle Vigen <kyle.vigen@clever.com>
2016-07-28 20:21:49 -0700

    Error more explicitly on things we don't support

--- main.go
+++ main.go
@@ -70,8 +70,70 @@ type SwaggerResponse struct {
 }
 
 type Swagger struct {
-	Definitions map[string]SwaggerDefinition           `yaml:"definitions"`
+	// TODO: Field to implement or error on if invalid
+	BasePath string                 `yaml:"basePath"`
+	Tags     map[string]interface{} `yaml:"tags"`
+
+	// Partially implemented
 	Paths       map[string]map[string]SwaggerOperation `yaml:"paths"`
+	Definitions map[string]SwaggerDefinition           `yaml:"definitions"`
+
+	// Fields we support, but only with a certain set of values
+	Swagger  string   `yaml:"swagger"`
+	Schemes  []string `yaml:"schemes"`
+	Consumes []string `yaml:"consumers"`
+	Produces []string `yaml:"produces"`
+
+	// Fields we don't support
+	Host                string                 `yaml:"host"`
+	Parameters          map[string]interface{} `yaml:"parameters"`
+	Responses           map[string]interface{} `yaml:"responses"`
+	SecurityDefinitions map[string]interface{} `yaml:"securityDefinitions"`
+	Security            map[string]interface{} `yaml:"security"`
+}
+
+// Validates returns an error if the swagger file is invalid or uses fields
+// we don't support. Note that this isn't a comprehensive check for all things
+// we don't support, so this may not return an error, but the Swagger file might
+// have values we don't support
+func (s Swagger) Validate() error {
+	if s.Swagger != "2.0" {
+		return fmt.Errorf("Unsupported Swagger version %s", s.Swagger)
+	}
+
+	if len(s.Schemes) != 1 || s.Schemes[0] != "http" {
+		return fmt.Errorf("Schemes only supports 'http'")
+	}
+
+	if len(s.Consumes) != 1 || s.Consumes[0] != "application/json" {
+		return fmt.Errorf("Consumes only supports 'application/json'")
+	}
+
+	if len(s.Produces) != 1 || s.Produces[0] != "application/json" {
+		return fmt.Errorf("Produces only supports 'application/json'")
+	}
+
+	if s.Host != "" {
+		return fmt.Errorf("Host parameter is not supported")
+	}
+
+	if len(s.Parameters) != 0 {
+		return fmt.Errorf("Global parameters definitions are not supported. Define parameters on a per request basis.")
+	}
+
+	if len(s.Responses) != 0 {
+		return fmt.Errorf("Global response definitions are not supported. Define responses on a per request basis")
+	}
+
+	if len(s.SecurityDefinitions) != 0 {
+		return fmt.Errorf("Security definitions definition not supported")
+	}
+
+	if len(s.Security) != 0 {
+		return fmt.Errorf("Security field not supported")
+	}
+
+	return nil
 }
 
 // TODO: Should this be a function on the swagger object directly?
