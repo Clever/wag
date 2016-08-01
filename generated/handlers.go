b7c3d8c5b3aad2ef41d20a862271acdbeaf856ab
Kyle Vigen <kyle.vigen@clever.com>
2016-08-01 15:12:21 -0700

    Add other input types to code generation

--- generated/handlers.go
+++ generated/handlers.go
@@ -80,8 +80,13 @@ func GetBookByIDHandler(ctx context.Context, w http.ResponseWriter, r *http.Requ
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(respBytes)
 }
+<<<<<<< a0005dbc4f1c70496590515a9cdd8b111d9685bb
 func CreateBookHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	input, err := NewCreateBookInput(r)
+=======
+func GetBookByIDHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	input, err := NewGetBookByIDInput(r)
+>>>>>>> Add other input types to code generation
 	if err != nil {
 		// TODO: Think about this whether this is usually an internal error or it could
 		// be from a bad request format...
@@ -95,10 +100,17 @@ func CreateBookHandler(ctx context.Context, w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+<<<<<<< a0005dbc4f1c70496590515a9cdd8b111d9685bb
 	resp, err := controller.CreateBook(ctx, input)
 	if err != nil {
 		if respErr, ok := err.(CreateBookError); ok {
 			http.Error(w, respErr.Error(), respErr.CreateBookStatusCode())
+=======
+	resp, err := controller.GetBookByID(ctx, input)
+	if err != nil {
+		if respErr, ok := err.(GetBookByIDError); ok {
+			http.Error(w, respErr.Error(), respErr.GetBookByIDStatusCode())
+>>>>>>> Add other input types to code generation
 			return
 		} else {
 			// This is the default case
@@ -107,7 +119,11 @@ func CreateBookHandler(ctx context.Context, w http.ResponseWriter, r *http.Reque
 		}
 	}
 
+<<<<<<< a0005dbc4f1c70496590515a9cdd8b111d9685bb
 	respBytes, err := json.Marshal(resp.CreateBookData())
+=======
+	respBytes, err := json.Marshal(resp.GetBookByIDData())
+>>>>>>> Add other input types to code generation
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
