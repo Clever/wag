1cc9095600620475c667bd396c623c562671dc00
Kyle Vigen <kyle.vigen@clever.com>
2016-08-05 14:05:48 -0700

    Very initial code

---
+++ test/sample_server.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"errors"
+	"net/http/httptest"
+
+	"github.com/Clever/inter-service-api-testing/codegen-poc/generated/models"
+	"github.com/Clever/inter-service-api-testing/codegen-poc/generated/server"
+	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
+)
+
+type ControllerImpl struct {
+}
+
+func (c ControllerImpl) GetBooks(ctx context.Context, input *models.GetBooksInput) (models.GetBooksOutput, error) {
+	// TODO: Implement me!
+	return nil, errors.New("Not implemented")
+}
+func (c ControllerImpl) GetBookByID(ctx context.Context, input *models.GetBookByIDInput) (models.GetBookByIDOutput, error) {
+	// TODO: Implement me!
+	return nil, errors.New("Not implemented")
+}
+func (c ControllerImpl) CreateBook(ctx context.Context, input *models.CreateBookInput) (models.CreateBookOutput, error) {
+	// TODO: Implement me!
+	return nil, errors.New("Not implemented")
+}
+
+func setupServer() *Server {
+	controller := ControllerImpl{}
+
+	router := server.SetupServer(mux.NewRouter(), controller)
+
+	return httptest.NewServer(router)
+}
