c43b5bc145afec8c44cbb065e26bcea2920b582e
Kyle Vigen <kyle.vigen@clever.com>
2016-08-08 15:55:26 -0700

    Working end-to-end tests

--- test/sample_server.go
+++ test/sample_server.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"errors"
 	"net/http/httptest"
 
 	"github.com/Clever/inter-service-api-testing/codegen-poc/generated/models"
@@ -11,25 +10,32 @@ import (
 )
 
 type ControllerImpl struct {
+	books map[int64]models.Book
 }
 
-func (c ControllerImpl) GetBooks(ctx context.Context, input *models.GetBooksInput) (models.GetBooksOutput, error) {
-	// TODO: Implement me!
-	return nil, errors.New("Not implemented")
+func (c *ControllerImpl) GetBooks(ctx context.Context, input *models.GetBooksInput) (models.GetBooksOutput, error) {
+	bookList := make([]models.Book, 0)
+	for _, book := range c.books {
+		bookList = append(bookList, book)
+	}
+	return models.GetBooks200Output(bookList), nil
 }
-func (c ControllerImpl) GetBookByID(ctx context.Context, input *models.GetBookByIDInput) (models.GetBookByIDOutput, error) {
-	// TODO: Implement me!
-	return nil, errors.New("Not implemented")
+func (c *ControllerImpl) GetBookByID(ctx context.Context, input *models.GetBookByIDInput) (models.GetBookByIDOutput, error) {
+	book, ok := c.books[input.BookID]
+	if !ok {
+		return nil, models.GetBookByID404Output{}
+	}
+	return models.GetBookByID200Output(book), nil
 }
-func (c ControllerImpl) CreateBook(ctx context.Context, input *models.CreateBookInput) (models.CreateBookOutput, error) {
-	// TODO: Implement me!
-	return nil, errors.New("Not implemented")
+func (c *ControllerImpl) CreateBook(ctx context.Context, input *models.CreateBookInput) (models.CreateBookOutput, error) {
+	c.books[input.NewBook.ID] = input.NewBook
+	return models.CreateBook200Output(input.NewBook), nil
 }
 
-func setupServer() *Server {
-	controller := ControllerImpl{}
+func setupServer() *httptest.Server {
+	controller := ControllerImpl{books: make(map[int64]models.Book)}
 
-	router := server.SetupServer(mux.NewRouter(), controller)
+	router := server.SetupServer(mux.NewRouter(), &controller)
 
 	return httptest.NewServer(router)
 }
