f0735adc1b8daaeb85fbdecaf107795101c0bf26
Kyle Vigen <kyle.vigen@clever.com>
2016-08-09 15:49:45 -0700

    Birthday

---
+++ generated/models/outputs.go
@@ -0,0 +1,119 @@
+package models
+
+// DefaultInternalError represents a generic 500 response.
+type DefaultInternalError struct {
+	Msg string `json:"msg"`
+}
+
+func (d DefaultInternalError) Error() string {
+	return d.Msg
+}
+
+type DefaultBadRequest struct {
+	Msg string `json:"msg"`
+}
+
+func (d DefaultBadRequest) Error() string {
+	return d.Msg
+}
+
+type GetBooksOutput interface {
+	GetBooksStatus() int
+	// Data is the underlying model object. We know it is json serializable
+	GetBooksData() interface{}
+}
+
+type GetBooksError interface {
+	error // Extend the error interface
+	GetBooksStatusCode() int
+}
+
+type GetBooks200Output []Book
+
+func (o GetBooks200Output) GetBooksData() interface{} {
+	return o
+}
+
+func (o GetBooks200Output) GetBooksStatus() int {
+	return 200
+}
+
+type GetBookByIDOutput interface {
+	GetBookByIDStatus() int
+	// Data is the underlying model object. We know it is json serializable
+	GetBookByIDData() interface{}
+}
+
+type GetBookByIDError interface {
+	error // Extend the error interface
+	GetBookByIDStatusCode() int
+}
+
+type GetBookByID200Output Book
+
+func (o GetBookByID200Output) GetBookByIDData() interface{} {
+	return o
+}
+
+func (o GetBookByID200Output) GetBookByIDStatus() int {
+	return 200
+}
+
+type GetBookByID204Output string
+
+func (o GetBookByID204Output) GetBookByIDData() interface{} {
+	return o
+}
+
+func (o GetBookByID204Output) GetBookByIDStatus() int {
+	return 204
+}
+
+type GetBookByID401Output string
+
+func (o GetBookByID401Output) GetBookByIDData() interface{} {
+	return o
+}
+
+func (o GetBookByID401Output) Error() string {
+	return "Status Code: " + "401"
+}
+
+func (o GetBookByID401Output) GetBookByIDStatusCode() int {
+	return 401
+}
+
+type GetBookByID404Output Error
+
+func (o GetBookByID404Output) GetBookByIDData() interface{} {
+	return o
+}
+
+func (o GetBookByID404Output) Error() string {
+	return "Status Code: " + "404"
+}
+
+func (o GetBookByID404Output) GetBookByIDStatusCode() int {
+	return 404
+}
+
+type CreateBookOutput interface {
+	CreateBookStatus() int
+	// Data is the underlying model object. We know it is json serializable
+	CreateBookData() interface{}
+}
+
+type CreateBookError interface {
+	error // Extend the error interface
+	CreateBookStatusCode() int
+}
+
+type CreateBook200Output Book
+
+func (o CreateBook200Output) CreateBookData() interface{} {
+	return o
+}
+
+func (o CreateBook200Output) CreateBookStatus() int {
+	return 200
+}
---
+++ generated/server/interface.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	"github.com/Clever/inter-service-api-testing/codegen-poc/generated/models"
+	"golang.org/x/net/context"
+)
+
+type Controller interface {
+	GetBooks(ctx context.Context, input *models.GetBooksInput) (models.GetBooksOutput, error)
+	GetBookByID(ctx context.Context, input *models.GetBookByIDInput) (models.GetBookByIDOutput, error)
+	CreateBook(ctx context.Context, input *models.CreateBookInput) (models.CreateBookOutput, error)
+}
