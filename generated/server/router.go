2e3adee359d379574deb343351db86ef4cb90cc8
Kyle Vigen <kyle.vigen@clever.com>
2016-08-10 21:30:16 -0700

    Update the generated code

--- generated/server/router.go
+++ generated/server/router.go
@@ -1,18 +1,37 @@
 package server
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 
 	gContext "github.com/gorilla/context"
 	"golang.org/x/net/context"
+
+	"gopkg.in/tylerb/graceful.v1"
 )
 
 type contextKey struct{}
 
-func SetupServer(r *mux.Router, c Controller) http.Handler {
+type Server struct {
+	Handler http.Handler
+	Port    int
+}
+
+func (s Server) Serve() error {
+	// Give the sever 30 seconds to shut down
+	graceful.Run(":"+string(s.Port), 30*time.Second, s.Handler)
+
+	// This should never return
+	return errors.New("This should never happen")
+}
+
+func New(c Controller, port int) Server {
 	controller = c // TODO: get rid of global variable?
+	r := mux.NewRouter()
+
 	r.Methods("GET").Path("/v1/books").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := gContext.Get(r, contextKey{}).(context.Context)
 		GetBooksHandler(ctx, w, r)
@@ -27,5 +46,6 @@ func SetupServer(r *mux.Router, c Controller) http.Handler {
 		ctx := gContext.Get(r, contextKey{}).(context.Context)
 		CreateBookHandler(ctx, w, r)
 	})
-	return withMiddleware(r)
+	handler := withMiddleware(r)
+	return Server{Handler: handler, Port: port}
 }
