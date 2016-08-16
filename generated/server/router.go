0cc47bba3edb839bb300c880e3aa8ead1c8565e8
Kyle Vigen <kyle.vigen@clever.com>
2016-08-15 17:49:44 -0700

    Get context from http.Request object
    This cleans up the middleware code quite a bit!

--- generated/server/router.go
+++ generated/server/router.go
@@ -6,9 +6,6 @@ import (
 
 	"github.com/gorilla/mux"
 
-	gContext "github.com/gorilla/context"
-	"golang.org/x/net/context"
-
 	"gopkg.in/tylerb/graceful.v1"
 )
 
@@ -29,18 +26,15 @@ func New(c Controller, addr string) Server {
 	r := mux.NewRouter()
 
 	r.Methods("GET").Path("/v1/books").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := gContext.Get(r, contextKey{}).(context.Context)
-		GetBooksHandler(ctx, w, r)
+		GetBooksHandler(r.Context(), w, r)
 	})
 
 	r.Methods("GET").Path("/v1/books/{bookID}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := gContext.Get(r, contextKey{}).(context.Context)
-		GetBookByIDHandler(ctx, w, r)
+		GetBookByIDHandler(r.Context(), w, r)
 	})
 
 	r.Methods("POST").Path("/v1/books/{bookID}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := gContext.Get(r, contextKey{}).(context.Context)
-		CreateBookHandler(ctx, w, r)
+		CreateBookHandler(r.Context(), w, r)
 	})
 	handler := withMiddleware(r)
 	return Server{Handler: handler, addr: addr}
