90378221691cb52976c0917361cc03e1cc3454d3
Kyle Vigen <kyle.vigen@clever.com>
2016-09-01 16:34:03 -0700

    Improve error messages a bit
    This change focuses on errors from validation

--- validation/validation.go
+++ validation/validation.go
@@ -11,30 +11,49 @@ import (
 var alphaNumRegex = regexp.MustCompile("^[a-zA-Z][a-zA-Z0-9]*$")
 
 // Validate checks if the swagger operation has any fields we don't support
-func validateOp(s *spec.Operation) error {
+func validateOp(path, op string, s *spec.Operation) error {
 	if len(s.Consumes) != 0 {
-		return fmt.Errorf("Consumes not supported in swagger operations")
+		return fmt.Errorf("%s %s cannot have a consumes field. WAG does not support the consumes field "+
+			"on operations", op, path)
 	}
 	if len(s.Produces) != 0 {
-		return fmt.Errorf("Produces not supported in swagger operations")
+		return fmt.Errorf("%s %s cannot have a produces field. WAG does not support the produces field "+
+			"on operations", op, path)
 	}
 	if len(s.Schemes) != 0 {
-		return fmt.Errorf("Schemes not supported in swagger operations")
+		return fmt.Errorf("%s %s cannot have a schemes field. WAG does not support the schemes field "+
+			"on operations", op, path)
 	}
 	if len(s.Security) != 0 {
-		return fmt.Errorf("Security not supported in swagger operations")
+		return fmt.Errorf("%s %s cannot have a security field. WAG does not support the security field "+
+			"on operations", op, path)
+	}
+
+	if s.ID == "" {
+		return fmt.Errorf("%s %s must have an operationId field, "+
+			"see http://swagger.io/specification/#operationObject", op, path)
 	}
 
 	if !alphaNumRegex.MatchString(s.ID) {
 		// We need this because we build function / struct names with the operationID.
 		// We could strip out the special characters, but it seems clearly to just enforce
 		// this.
-		return fmt.Errorf("OperationIDs must be alphanumeric and start with a letter")
+		return fmt.Errorf("The operationId for %s %s must be alphanumeric and start with a letter",
+			op, path)
 	}
 
 	for _, param := range s.Parameters {
 		if param.In == "path" && !param.Required {
-			return fmt.Errorf("Path parameters must be required")
+			return fmt.Errorf("%s for %s %s is a path parameter so it must be required",
+				param.Name, op, path)
+		}
+
+		if param.Type == "string" && param.Format != "" {
+			if param.MaxLength != nil || param.MinLength != nil || param.Pattern != "" || len(param.Enum) > 0 {
+				return fmt.Errorf("%s for %s %s cannot have min/max length, pattern, or enum fields. "+
+					"Only string type parameters without a format can have additional validation.",
+					param.Name, op, path)
+			}
 		}
 	}
 
@@ -51,47 +70,48 @@ func Validate(s spec.Swagger) error {
 	}
 
 	if len(s.Schemes) != 1 || s.Schemes[0] != "http" {
-		return fmt.Errorf("Schemes only supports 'http'")
+		return fmt.Errorf("WAG only supports the scheme 'http'")
 	}
 
 	if len(s.Consumes) > 1 || (len(s.Produces) == 0 && s.Consumes[0] != "application/json") {
-		return fmt.Errorf("Consumes only supports 'application/json'")
+		return fmt.Errorf("WAG only supports the consumes option: 'application/json'")
 	}
 
 	if len(s.Produces) > 1 || (len(s.Produces) == 0 && s.Produces[0] != "application/json") {
-		return fmt.Errorf("Produces only supports 'application/json'")
+		return fmt.Errorf("WAG only support the consumes option: 'application/json'")
 	}
 
 	if s.Host != "" {
-		return fmt.Errorf("Host parameter is not supported")
+		return fmt.Errorf("WAG does not support the host field.")
 	}
 
 	if len(s.Parameters) != 0 {
-		return fmt.Errorf("Global parameters definitions are not supported. Define parameters on a per request basis.")
+		return fmt.Errorf("WAG does not support global parameters definitions. Define parameters on a per request basis.")
 	}
 
 	if len(s.Responses) != 0 {
-		return fmt.Errorf("Global response definitions are not supported. Define responses on a per request basis")
+		return fmt.Errorf("WAG does not support global response definitions.  Define responses on a per request basis")
 	}
 
 	if len(s.SecurityDefinitions) != 0 {
-		return fmt.Errorf("Security definitions definition not supported")
+		return fmt.Errorf("WAG does not support the security definitions field.")
 	}
 
 	if len(s.Security) != 0 {
-		return fmt.Errorf("Security field not supported")
+		return fmt.Errorf("WAG does not support the security field.")
 	}
 
-	for _, pathItem := range s.Paths.Paths {
+	for path, pathItem := range s.Paths.Paths {
 		if pathItem.Ref.String() != "" {
-			return fmt.Errorf("Paths cannot have $ref fields")
+			return fmt.Errorf("WAG does not support paths with $ref fields. Define the references on " +
+				"a per operation basis")
 		}
 		if len(pathItem.Parameters) != 0 {
 			return fmt.Errorf("Parameters cannot be defined for an entire path. " +
 				"They must be defined on the individual method level.")
 		}
-		for _, op := range pathItemOperations(pathItem) {
-			if err := validateOp(op); err != nil {
+		for op, opItem := range pathItemOperations(pathItem) {
+			if err := validateOp(path, op, opItem); err != nil {
 				return err
 			}
 		}
--- validation/validation_test.go
+++ validation/validation_test.go
@@ -10,18 +10,20 @@ import (
 func TestValidateOpID(t *testing.T) {
 	op := spec.Operation{}
 	op.ID = "with spaces"
-	err := validateOp(&op)
+	err := validateOp("/books", "GET", &op)
 	assert.Error(t, err)
-	assert.Equal(t, "OperationIDs must be alphanumeric and start with a letter", err.Error())
+	assert.Equal(t, "The operationId for GET /books must be alphanumeric and start with a letter", err.Error())
 }
 
 func TestValidatePathParams(t *testing.T) {
 	op := spec.Operation{}
+	op.ID = "op"
 	param := spec.Parameter{}
+	param.Name = "paramName"
 	param.In = "path"
 	param.Required = false
 	op.Parameters = []spec.Parameter{param}
-	err := validateOp(&op)
+	err := validateOp("/books", "GET", &op)
 	assert.Error(t, err)
-	assert.Equal(t, "OperationIDs must be alphanumeric and start with a letter", err.Error())
+	assert.Equal(t, "paramName for GET /books is a path parameter so it must be required", err.Error())
 }
