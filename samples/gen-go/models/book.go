51462bcef521cfb20bb106e6c636f9479fb3bd67
Kyle Vigen <kyle.vigen@clever.com>
2016-10-17 19:19:37 -0700

    Example of using additional properties

--- samples/gen-go/models/book.go
+++ samples/gen-go/models/book.go
@@ -5,8 +5,10 @@ package models
 
 import (
 	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
 
 	"github.com/go-openapi/errors"
+	"github.com/go-openapi/validate"
 )
 
 // Book book
@@ -21,14 +23,35 @@ type Book struct {
 
 	// name
 	Name string `json:"name,omitempty"`
+
+	// other
+	Other map[string]string `json:"other,omitempty"`
 }
 
 // Validate validates this book
 func (m *Book) Validate(formats strfmt.Registry) error {
 	var res []error
 
+	if err := m.validateOther(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+func (m *Book) validateOther(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Other) { // not required
+		return nil
+	}
+
+	if err := validate.Required("other", "body", m.Other); err != nil {
+		return err
+	}
+
 	return nil
 }
