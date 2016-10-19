866e683ab00c77d2169630a1e781614a4c16785e
Kyle Vigen <kyle.vigen@clever.com>
2016-10-19 12:51:10 -0700

    Add test

---
+++ test/no_definitions_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"context"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Clever/wag/gen-no-definitions/client"
+	"github.com/Clever/wag/gen-no-definitions/models"
+	"github.com/Clever/wag/gen-no-definitions/server"
+	"github.com/stretchr/testify/assert"
+)
+
+type NoDefinitionsController struct{}
+
+func (m *NoDefinitionsController) DeleteBook(ctx context.Context, i *models.DeleteBookInput) error {
+	if i.ID == 404 {
+		return models.DeleteBook404Output{}
+	}
+	return nil
+}
+
+func TestNoDefinitions(t *testing.T) {
+	s := server.New(&NoDefinitionsController{}, "")
+	testServer := httptest.NewServer(testContextMiddleware(s.Handler))
+	c := client.New(testServer.URL)
+
+	// Success
+	err := c.DeleteBook(context.Background(), &models.DeleteBookInput{})
+	assert.NoError(t, err)
+
+	// Failure
+	err = c.DeleteBook(context.Background(), &models.DeleteBookInput{ID: 404})
+	assert.Error(t, err)
+	assert.IsType(t, models.DeleteBook404Output{}, err)
+}
