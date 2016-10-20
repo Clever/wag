370351fabbd8f7f29326b334b99224e115133243
Kyle Vigen <kyle.vigen@clever.com>
2016-10-20 12:26:59 -0700

    Base
    Before my changes

---
+++ samples/gen-wag-patch/server/handlers.go
@@ -0,0 +1,147 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/Clever/wag/samples/gen-wag-patch/models"
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+	"github.com/gorilla/mux"
+	"gopkg.in/Clever/kayvee-go.v5/logger"
+)
+
+var _ = strconv.ParseInt
+var _ = strfmt.Default
+var _ = swag.ConvertInt32
+var _ = errors.New
+var _ = mux.Vars
+var _ = bytes.Compare
+var _ = ioutil.ReadAll
+
+var formats = strfmt.Default
+var _ = formats
+
+// convertBase64 takes in a string and returns a strfmt.Base64 if the input
+// is valid base64 and an error otherwise.
+func convertBase64(input string) (strfmt.Base64, error) {
+	temp, err := formats.Parse("byte", input)
+	if err != nil {
+		return strfmt.Base64{}, err
+	}
+	return *temp.(*strfmt.Base64), nil
+}
+
+// convertDateTime takes in a string and returns a strfmt.DateTime if the input
+// is a valid DateTime and an error otherwise.
+func convertDateTime(input string) (strfmt.DateTime, error) {
+	temp, err := formats.Parse("date-time", input)
+	if err != nil {
+		return strfmt.DateTime{}, err
+	}
+	return *temp.(*strfmt.DateTime), nil
+}
+
+// convertDate takes in a string and returns a strfmt.Date if the input
+// is a valid Date and an error otherwise.
+func convertDate(input string) (strfmt.Date, error) {
+	temp, err := formats.Parse("date", input)
+	if err != nil {
+		return strfmt.Date{}, err
+	}
+	return *temp.(*strfmt.Date), nil
+}
+
+func jsonMarshalNoError(i interface{}) string {
+	bytes, err := json.Marshal(i)
+	if err != nil {
+		// This should never happen
+		return ""
+	}
+	return string(bytes)
+}
+
+// statusCodeForWagpatch returns the status code corresponding to the returned
+// object. It returns -1 if the type doesn't correspond to anything.
+func statusCodeForWagpatch(obj interface{}) int {
+
+	switch obj.(type) {
+
+	case *models.Data:
+		return 200
+
+	case models.Data:
+		return 200
+
+	case models.DefaultBadRequest:
+		return 400
+	case models.DefaultInternalError:
+		return 500
+	default:
+		return -1
+	}
+}
+
+func (h handler) WagpatchHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+
+	input, err := newWagpatchInput(r)
+	if err != nil {
+		logger.FromContext(ctx).AddContext("error", err.Error())
+		http.Error(w, jsonMarshalNoError(models.DefaultBadRequest{Msg: err.Error()}), http.StatusBadRequest)
+		return
+	}
+
+	err = input.Validate(nil)
+	if err != nil {
+		logger.FromContext(ctx).AddContext("error", err.Error())
+		http.Error(w, jsonMarshalNoError(models.DefaultBadRequest{Msg: err.Error()}), http.StatusBadRequest)
+		return
+	}
+
+	resp, err := h.Wagpatch(ctx, input)
+
+	if err != nil {
+		logger.FromContext(ctx).AddContext("error", err.Error())
+		statusCode := statusCodeForWagpatch(err)
+		if statusCode != -1 {
+			http.Error(w, err.Error(), statusCode)
+		} else {
+			http.Error(w, jsonMarshalNoError(models.DefaultInternalError{Msg: err.Error()}), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	respBytes, err := json.Marshal(resp)
+	if err != nil {
+		logger.FromContext(ctx).AddContext("error", err.Error())
+		http.Error(w, jsonMarshalNoError(models.DefaultInternalError{Msg: err.Error()}), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCodeForWagpatch(resp))
+	w.Write(respBytes)
+
+}
+
+// newWagpatchInput takes in an http.Request an returns the input struct.
+func newWagpatchInput(r *http.Request) (*models.Data, error) {
+	var input models.Data
+
+	var err error
+	_ = err
+
+	data, err := ioutil.ReadAll(r.Body)
+	if len(data) > 0 {
+		if err := json.NewDecoder(bytes.NewReader(data)).Decode(&input); err != nil {
+			return nil, err
+		}
+	}
+
+	return &input, nil
+}
---
+++ samples/gen-wag-patch/server/router.go
@@ -0,0 +1,63 @@
+package server
+
+// Code auto-generated. Do not edit.
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+
+	"github.com/Clever/go-process-metrics/metrics"
+	"github.com/gorilla/mux"
+	"gopkg.in/Clever/kayvee-go.v5/logger"
+	"gopkg.in/tylerb/graceful.v1"
+)
+
+type contextKey struct{}
+
+// Server defines a HTTP server that implements the Controller interface.
+type Server struct {
+	// Handler should generally not be changed. It exposed to make testing easier.
+	Handler http.Handler
+	addr    string
+	l       *logger.Logger
+}
+
+// Serve starts the server. It will return if an error occurs.
+func (s *Server) Serve() error {
+
+	go func() {
+		metrics.Log("wag-patch", 1*time.Minute)
+	}()
+
+	go func() {
+		// This should never return. Listen on the pprof port
+		log.Printf("PProf server crashed: %s", http.ListenAndServe(":6060", nil))
+	}()
+
+	s.l.Counter("server-started")
+
+	// Give the sever 30 seconds to shut down
+	return graceful.RunWithErr(s.addr, 30*time.Second, s.Handler)
+}
+
+type handler struct {
+	Controller
+}
+
+// New returns a Server that implements the Controller interface. It will start when "Serve" is called.
+func New(c Controller, addr string) *Server {
+	r := mux.NewRouter()
+	h := handler{Controller: c}
+
+	l := logger.New("wag-patch")
+
+	r.Methods("PATCH").Path("/wagpatch").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.FromContext(r.Context()).AddContext("op", "wagpatch")
+		h.WagpatchHandler(r.Context(), w, r)
+	})
+
+	handler := withMiddleware("wag-patch", r)
+	return &Server{Handler: handler, addr: addr, l: l}
+}
