b993807047c836b5d482601501fadebabc14feb3
Rafael <rafael.garcia@clever.com>
2016-11-02 14:47:21 -0700

    circuit breaker in clients (#81)

--- clients/go/gengo.go
+++ clients/go/gengo.go
@@ -43,9 +43,12 @@ import (
 		"encoding/json"
 		"strconv"
 		"time"
-		"{{.PackageName}}/models"
+		"fmt"
+		"crypto/md5"
 
+		"{{.PackageName}}/models"
 		discovery "github.com/Clever/discovery-go"
+		"github.com/afex/hystrix-go/hystrix"
 )
 
 var _ = json.Marshal
@@ -61,6 +64,8 @@ type WagClient struct {
 	timeout     time.Duration
 	// Keep the retry doer around so that we can set the number of retries
 	retryDoer *retryDoer
+	// Keep the circuit doer around so that we can turn it on / off
+	circuitDoer    *circuitBreakerDoer
 	defaultTimeout time.Duration
 }
 
@@ -71,9 +76,17 @@ func New(basePath string) *WagClient {
 	base := baseDoer{}
 	tracing := tracingDoer{d: base}
 	retry := retryDoer{d: tracing, defaultRetries: 1}
-
-	return &WagClient{requestDoer: &retry, retryDoer: &retry, defaultTimeout: 10 * time.Second,
-		transport: &http.Transport{}, basePath: basePath}
+	circuit := &circuitBreakerDoer{
+		d:     &retry,
+		debug: true,
+		// one circuit for each service + url pair
+		circuitName: fmt.Sprintf("{{.ServiceName}}-%%s", shortHash(basePath)),
+	}
+	circuit.init()
+	client := &WagClient{requestDoer: circuit, retryDoer: &retry, circuitDoer: circuit, defaultTimeout: 10 * time.Second,
+ 		transport: &http.Transport{}, basePath: basePath}
+	client.SetCircuitBreakerSettings(DefaultCircuitBreakerSettings)
+	return client
 }
 
 // NewFromDiscovery creates a client from the discovery environment variables. This method requires
@@ -96,6 +109,50 @@ func (c *WagClient) WithRetries(retries int) *WagClient {
 	return c
 }
 
+// SetCircuitBreakerDebug puts the circuit
+func (c *WagClient) SetCircuitBreakerDebug(b bool) {
+	c.circuitDoer.debug = b
+}
+
+// CircuitBreakerSettings are the parameters that govern the client's circuit breaker.
+type CircuitBreakerSettings struct {
+	// MaxConcurrentRequests is the maximum number of concurrent requests
+	// the client can make at the same time. Default: 100.
+	MaxConcurrentRequests int
+	// RequestVolumeThreshold is the minimum number of requests needed
+	// before a circuit can be tripped due to health. Default: 20.
+	RequestVolumeThreshold int
+	// SleepWindow how long, in milliseconds, to wait after a circuit opens
+	// before testing for recovery. Default: 5000.
+	SleepWindow int
+	// ErrorPercentThreshold is the threshold to place on the rolling error
+	// rate. Once the error rate exceeds this percentage, the circuit opens.
+	// Default: 90.
+	ErrorPercentThreshold int
+}
+
+// DefaultCircuitBreakerSettings describes the default circuit parameters.
+var DefaultCircuitBreakerSettings = CircuitBreakerSettings{
+	MaxConcurrentRequests:  100,
+	RequestVolumeThreshold: 20,
+	SleepWindow:            5000,
+	ErrorPercentThreshold:  90,
+}
+
+// SetCircuitBreakerSettings sets parameters on the circuit breaker. It must be
+// called on application startup.
+func (c *WagClient) SetCircuitBreakerSettings(settings CircuitBreakerSettings) {
+	hystrix.ConfigureCommand(c.circuitDoer.circuitName, hystrix.CommandConfig{
+		// redundant, with the timeout we set on the context, so set
+		// this to something high and irrelevant
+		Timeout:                100 * 1000,
+		MaxConcurrentRequests:  settings.MaxConcurrentRequests,
+		RequestVolumeThreshold: settings.RequestVolumeThreshold,
+		SleepWindow:            settings.SleepWindow,
+		ErrorPercentThreshold:  settings.ErrorPercentThreshold,
+	})
+}
+
 // WithTimeout returns a new client that has the specified timeout on all operations. To make a single request
 // have a timeout use context.WithTimeout as described here: https://godoc.org/golang.org/x/net/context#WithTimeout.
 func (c *WagClient) WithTimeout(timeout time.Duration) *WagClient {
@@ -109,6 +166,9 @@ func (c *WagClient) WithTimeout(timeout time.Duration) *WagClient {
 	{{$methodCode}}
 {{end}}
 
+func shortHash(s string) string {
+	return fmt.Sprintf("%%x", md5.Sum([]byte(s)))[0:6]
+}
 `
 
 func generateClient(packageName string, s spec.Swagger) error {
