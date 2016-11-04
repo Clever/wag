75f9995b5dcefe51bb6dfce9cc3ff47d5947c1d9
Kyle Vigen <kyle.vigen@clever.com>
2016-11-03 17:55:28 -0700

    More transform logic
    Close to done

---
+++ swagger/transforms_test.go
@@ -0,0 +1,73 @@
+package swagger
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/loads/fmts"
+	"github.com/go-openapi/spec"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBadReference(t *testing.T) {
+	badRef, err := createRefResponse("test", "badRef")
+	require.NoError(t, err)
+
+	s := spec.Swagger{
+		SwaggerProps: spec.SwaggerProps{
+			Responses: map[string]spec.Response{
+				"BadRequest": *badRef,
+			},
+		},
+	}
+
+	err = TransformErrors(s)
+	require.Error(t, err)
+	assert.True(t, strings.Contains(err.Error(), "invalid schema reference"), err.Error())
+}
+
+func TestReferenceMissingMsgField(t *testing.T) {
+	ref, err := createRefResponse("test", "#/definitions/Test")
+	require.NoError(t, err)
+
+	s := spec.Swagger{
+		SwaggerProps: spec.SwaggerProps{
+			Responses: map[string]spec.Response{
+				"BadRequest": *ref,
+			},
+			Definitions: map[string]spec.Schema{
+				"Test": spec.Schema{},
+			},
+		},
+	}
+
+	err = TransformErrors(s)
+	assert.Error(t, err)
+	assert.True(t, strings.Contains(err.Error(), "'msg' field"), err.Error())
+}
+
+func TestErrorOnMissingTypes(t *testing.T) {
+	// TODO: Implement me!!!
+}
+
+func loadTestFile(t *testing.T, filename string) spec.Swagger {
+	loads.AddLoader(fmts.YAMLMatcher, fmts.YAMLDoc)
+	doc, err := loads.Spec(filename)
+	require.NoError(t, err)
+	return *doc.Spec()
+}
+
+func TestAddingDefaultTypes(t *testing.T) {
+	s := loadTestFile(t, "testyml/defaults.yml")
+	assert.NoError(t, TransformErrors(s))
+
+	responses := s.Paths.Paths["/path"].Get.Responses.StatusCodeResponses
+	require.Equal(t, 3, len(responses))
+}
+
+func TestOverrideDefaults(t *testing.T) {
+	s := loadTestFile(t, "testyml/override.yml")
+	assert.NoError(t, TransformErrors(s))
+}
