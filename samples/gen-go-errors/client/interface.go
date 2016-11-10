0c088419b23855f13e8e15fd8401bd07be31b098
Kyle Vigen <kyle.vigen@clever.com>
2016-11-10 14:44:06 -0800

    Generated code

--- samples/gen-go-errors/client/interface.go
+++ samples/gen-go-errors/client/interface.go
@@ -11,6 +11,12 @@ import (
 // Client defines the methods available to clients of the swagger-test service.
 type Client interface {
 
-	// GetBook makes a GET request to /books/{id}.
+	// GetBook makes a GET request to /books/{id}
+	//
+	// 200: nil
+	// 400: *models.ExtendedError
+	// 404: *models.NotFound
+	// 500: *models.InternalError
+	// default: client side HTTP errors, for example: context.DeadlineExceeded.
 	GetBook(ctx context.Context, i *models.GetBookInput) error
 }
