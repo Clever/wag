5a7dbf7fc42877f867e75838aa0f16853abc4297
Sam Fishman <sam.fishman@clever.com>
2017-02-13 20:14:15 +0000

    test: Add test for next page header.

--- test/sample_server.go
+++ test/sample_server.go
@@ -11,16 +11,31 @@ import (
 
 // ControllerImpl implements the test server controller interface.
 type ControllerImpl struct {
-	books map[int64]*models.Book
+	books    []*models.Book
+	pageSize int
 }
 
 // GetBooks returns a list of books.
 func (c *ControllerImpl) GetBooks(ctx context.Context, input *models.GetBooksInput) ([]models.Book, int64, error) {
 	var bookList []models.Book
-	for _, book := range c.books {
-		bookList = append(bookList, *book)
+
+	begin := 0
+	if input.StartingAfter != nil {
+		begin = int(*input.StartingAfter) + 1
 	}
-	return bookList, int64(0), nil
+
+	nextPage := int64(0) // default to no next page
+	for idx, book := range c.books[begin:] {
+		if book != nil {
+			bookList = append(bookList, *book)
+			if len(bookList) == c.pageSize {
+				nextPage = int64(idx)
+				break
+			}
+		}
+	}
+
+	return bookList, nextPage, nil
 }
 
 // GetBookByID returns a book by ID.
@@ -29,8 +44,11 @@ func (c *ControllerImpl) GetBookByID(ctx context.Context, input *models.GetBookB
 		return nil, models.BadRequest{Message: "My 400 failure"}
 	}
 
-	book, ok := c.books[input.BookID]
-	if !ok {
+	if int(input.BookID) >= len(c.books) {
+		return nil, models.Error{}
+	}
+	book := c.books[input.BookID]
+	if book == nil {
 		return nil, models.Error{}
 	}
 	return book, nil
@@ -42,8 +60,11 @@ func (c *ControllerImpl) GetBookByID2(ctx context.Context, id string) (*models.B
 	if err != nil {
 		return nil, err
 	}
-	book, ok := c.books[int64(i)]
-	if !ok {
+	if i >= len(c.books) {
+		return nil, models.Error{}
+	}
+	book := c.books[i]
+	if book == nil {
 		return nil, models.Error{}
 	}
 	return book, nil
@@ -51,6 +72,10 @@ func (c *ControllerImpl) GetBookByID2(ctx context.Context, id string) (*models.B
 
 // CreateBook creates a book.
 func (c *ControllerImpl) CreateBook(ctx context.Context, input *models.Book) (*models.Book, error) {
+	if int(input.ID) >= len(c.books) {
+		extension := make([]*models.Book, int(input.ID)-len(c.books)+1)
+		c.books = append(c.books, extension...)
+	}
 	c.books[input.ID] = input
 	return input, nil
 }
@@ -60,10 +85,10 @@ func (c *ControllerImpl) HealthCheck(ctx context.Context) error {
 	return nil
 }
 
-func setupServer() *httptest.Server {
-	controller := ControllerImpl{books: make(map[int64]*models.Book)}
+func setupServer() (*httptest.Server, *ControllerImpl) {
+	controller := ControllerImpl{pageSize: 100}
 
 	s := server.New(&controller, ":8080")
 
-	return httptest.NewServer(s.Handler)
+	return httptest.NewServer(s.Handler), &controller
 }
