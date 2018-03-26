8931408c67849df4a018edee4d7915ff33540473
Kyle Vigen <kyle.vigen@clever.com>
2018-03-26 11:12:14 -0700

    Generated code

---
+++ samples/gen-go-blog/server/interface.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"context"
+
+	"github.com/Clever/wag/samples/gen-go-blog/models"
+)
+
+//go:generate $GOPATH/bin/mockgen -source=$GOFILE -destination=mock_controller.go -package=server
+
+// Controller defines the interface for the blog service.
+type Controller interface {
+
+	// GetSectionsForStudent handles POST requests to /students/{student_id}/sections
+	// Gets the sections for the specified student
+	// 200: *models.Section
+	// 400: *models.BadRequest
+	// 500: *models.InternalError
+	// default: client side HTTP errors, for example: context.DeadlineExceeded.
+	GetSectionsForStudent(ctx context.Context, studentID string) (*models.Section, error)
+}
---
+++ samples/gen-js-blog/types.js
@@ -0,0 +1,34 @@
+module.exports.Errors = {};
+
+/**
+ * BadRequest
+ * @extends Error
+ * @memberof module:blog
+ * @alias module:blog.Errors.BadRequest
+ * @property {string} message
+ */
+module.exports.Errors.BadRequest = class extends Error {
+  constructor(body) {
+    super(body.message);
+    for (const k of Object.keys(body)) {
+      this[k] = body[k];
+    }
+  }
+};
+
+/**
+ * InternalError
+ * @extends Error
+ * @memberof module:blog
+ * @alias module:blog.Errors.InternalError
+ * @property {string} message
+ */
+module.exports.Errors.InternalError = class extends Error {
+  constructor(body) {
+    super(body.message);
+    for (const k of Object.keys(body)) {
+      this[k] = body[k];
+    }
+  }
+};
+
