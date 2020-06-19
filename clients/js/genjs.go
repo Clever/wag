d7aec43aa83dfe6c4d2cde0a9f5490618634362f
Adam Victor <adam.victor@clever.com>
2020-06-18 21:19:54 -0700

    Add jsdocs for static content

--- clients/js/genjs.go
+++ clients/js/genjs.go
@@ -1335,8 +1335,11 @@ interface RetryPolicy {
 }
 
 interface RequestOptions {
-  timeout?: number;
-  span?: Span;
+	/** The timeout to use for all client requests, in milliseconds. */
+	timeout?: number;
+	/** An OpenTracing span - For example from the parent request */
+	span?: Span;
+	/** The logic to determine which requests to retry, as well as how many times to retry. */
   retryPolicy?: RetryPolicy;
 }
 
@@ -1348,30 +1351,44 @@ interface IterResult<R> {
 }
 
 interface CircuitOptions {
-  forceClosed?: boolean;
-  maxConcurrentRequests?: number;
-  requestVolumeThreshold?: number;
-  sleepWindow?: number;
+	/** When set to true the circuit will always be closed. Default: true. */
+	forceClosed?: boolean;
+	/** The maximum number of concurrent requests the client can make at the same time. Default: 100. */
+	maxConcurrentRequests?: number;
+	/** The minimum number of requests needed before a circuit can be tripped due to health. Default: 20. */
+	requestVolumeThreshold?: number;
+	/** How long, in milliseconds, to wait after a circuit opens before testing for recovery. Default: 5000. */
+	sleepWindow?: number;
+	/** The threshold to place on the rolling error rate. Once the error rate exceeds this percentage, the circuit opens. Default: 90. */
   errorPercentThreshold?: number;
 }
 
 interface GenericOptions {
-  timeout?: number;
-  keepalive?: boolean;
-  retryPolicy?: RetryPolicy;
-  logger?: Logger;
-  tracer?: Tracer;
-  circuit?: CircuitOptions;
+	/** The timeout to use for all client requests, in milliseconds. This can be overridden on a per-request basis. Default is 5000ms. */
+	timeout?: number;
+	/** Set keepalive to true for client requests. This sets the forever: true attribute in request. Defaults to true. */
+	keepalive?: boolean;
+	/** The logic to determine which requests to retry, as well as how many times to retry. */
+	retryPolicy?: RetryPolicy;
+	/** The Kayvee logger to use in the client. */
+	logger?: Logger;
+	/** The OpenTracing Tracer to use. Defaults to the OpenTracing globalTracer. */
+	tracer?: Tracer;
+	/** Options for constructing the client's circuit breaker. */
+	circuit?: CircuitOptions;
+	/** Overrides the default service name. This is necessary if the same client is used multiple times, but with different settings, such as with sso- clients. */
   serviceName?: string;
 }
 
 interface DiscoveryOptions {
-  discovery: true;
+	/** Use clever-discovery to locate the server. Must provide this or the address argument. */
+	discovery: true;
   address?: undefined;
 }
 
 interface AddressOptions {
-  discovery?: false;
+	discovery?: false;
+	/** URL where the server is located. Must provide this or the discovery argument. */
   address: string;
 }
 
@@ -1380,6 +1397,10 @@ type {{.ServiceName}}Options = (DiscoveryOptions | AddressOptions) & GenericOpti
 import models = {{.ServiceName}}.Models
 
 declare class {{.ServiceName}} {
+	/**
+	* Create a new client object.
+	* @param options - Options for constructing a client object.
+	*/
   constructor(options: {{.ServiceName}}Options);
 
   {{range .MethodDecls}}
