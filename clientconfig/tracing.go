4cec182a5fe891078b5b4f9537b6ff063f21e3c3
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2023-12-21 15:59:16 -0500

    with no app renaming

--- clientconfig/tracing.go
+++ clientconfig/tracing.go
@@ -83,9 +83,9 @@ func (rt roundTripperWithTracing) RoundTrip(r *http.Request) (*http.Response, er
 	return otelhttp.NewTransport(
 		rt.baseTransport,
 		otelhttp.WithTracerProvider(otel.GetTracerProvider()),
-		otelhttp.WithServiceName(rt.appName),
 		otelhttp.WithPropagators(propagator),
 		otelhttp.WithSpanNameFormatter(func(method string, r *http.Request) string {
+
 			v, ok := r.Context().Value("otelSpanName").(string)
 			if ok {
 				return v
