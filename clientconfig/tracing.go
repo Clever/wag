315c216eb3a325e46facaa5726e83f847d5bb612
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2024-02-20 20:27:18 -0500

    cleanup tracing.go

--- clientconfig/tracing.go
+++ clientconfig/tracing.go
@@ -5,33 +5,7 @@ import (
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
 )
-
-var propagator propagation.TextMapPropagator = propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}) // traceparent header
-
-type Option interface {
-	apply(*options)
-}
-type options struct {
-	address string
-}
-
-// WithAddress takes an address in the form of Host:Port
-// Leaving this here as an example of how to add a new option.
-// This was removed because we shouldn't be adjusting the exporter for a client connection.
-
-// func WithAddress(addr string) Option {
-// 	return addrOption{address: addr}
-// }
-
-// type addrOption struct {
-// 	address string
-// }
-
-// func (o addrOption) apply(opts *options) {
-// 	opts.address = o.address
-// }
 
 // DefaultInstrumentor returns the transport to use in client requests.
 // It takes in a transport to wrap, e.g. http.DefaultTransport, and the request
@@ -50,13 +24,16 @@ func (rt roundTripperWithTracing) RoundTrip(r *http.Request) (*http.Response, er
 	return otelhttp.NewTransport(
 		rt.baseTransport,
 		otelhttp.WithTracerProvider(otel.GetTracerProvider()),
-		otelhttp.WithPropagators(propagator),
+		otelhttp.WithPropagators(otel.GetTextMapPropagator()),
 		otelhttp.WithSpanNameFormatter(func(method string, r *http.Request) string {
-
+			// I passed in appName because ideally I'd also include it here in the span name.
+			// Currently left out to avoid other possible issues.
 			v, ok := r.Context().Value("otelSpanName").(string)
 			if ok {
 				return v
 			}
+			// return fmt.Sprintf("%s-wagclient %s %s", rt.appName, r.Method, r.URL.Path)
+
 			return r.Method
 		}),
 	).RoundTrip(r)
