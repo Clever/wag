385e0f7ce516ab78aa8004ee10f0dc0ffee06274
Sophia Lanzendorf <sophia.lanzendorf@clever.com>
2025-11-19 10:10:18 -0500

    chore: make generate

--- samples/gen-js-nils/index.js
+++ samples/gen-js-nils/index.js
@@ -98,7 +98,7 @@ function responseLog(logger, req, res, err) {
     "message": err || (res.statusMessage || ""),
     "status_code": res.statusCode || 0,
   };
-  
+
   if (err) {
 	if (logData.status_code <= 499){
 		logger.warnD("client-request-finished", logData);
@@ -178,7 +178,7 @@ class NilTest {
    * @param {number} [options.circuit.errorPercentThreshold] - the threshold to place on the rolling error
    * rate. Once the error rate exceeds this percentage, the circuit opens.
    * Default: 90.
-   * @param {object} [options.asynclocalstore] a request scoped async store 
+   * @param {object} [options.asynclocalstore] a request scoped async store
    */
   constructor(options) {
     options = options || {};
@@ -219,7 +219,7 @@ class NilTest {
 
     const circuitOptions = Object.assign({}, defaultCircuitOptions, options.circuit);
     // hystrix implements a caching mechanism, we don't want this or we can't trust that clients
-    // are initialized with the values passed in. 
+    // are initialized with the values passed in.
     commandFactory.resetCache();
     circuitFactory.resetCache();
     metricsFactory.resetCache();
@@ -314,7 +314,7 @@ class NilTest {
       if (!options) {
         options = {};
       }
-  
+
       const optionsBaggage = options.baggage || new Map();
 
       const storeContext = this.asynclocalstore?.get("context") || new Map();
@@ -324,10 +324,10 @@ class NilTest {
       const timeout = options.timeout || this.timeout;
 
       let headers = {};
-      
+
       // Convert combinedContext into a string using parseForBaggage
       headers["baggage"] = parseForBaggage(combinedContext);
-      
+
       headers["Canonical-Resource"] = "nilCheck";
       headers[versionHeader] = version;
       if (!params.id) {
